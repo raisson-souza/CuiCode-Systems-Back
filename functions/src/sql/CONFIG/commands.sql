a5cf5b70cae0bf37176ad70d2c4a3ec14b5b5d9c
Raisson Souza <raissonrai@gmail.com>
2023-08-13 19:21:24 -0300

    - SQLs added.

---
+++ functions/src/sql/CONFIG/commands.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE cuicode_systems
+
+CREATE SCHEMA production
+
+CREATE SCHEMA staging
+
+CREATE SCHEMA testing
---
+++ functions/src/sql/GROUPS/commands.sql
@@ -0,0 +1,25 @@
+-- CUIDAR SCHEMA
+
+CREATE TABLE IF NOT EXISTS "groups"(
+	id SERIAL,
+	"name" varchar(100),
+	creator_id int,
+	"type" int,
+	created_date DATE,
+	active boolean,
+	deleted_date DATE,
+	deleted DATE,
+	PRIMARY KEY (id),
+	FOREIGN KEY (creator_id) REFERENCES users (id)
+);
+
+CREATE TABLE IF NOT EXISTS groups_relation(
+	id SERIAL,
+	user_id int,
+	group_id int,
+	entry_date DATE,
+	deleted_date DATE,
+	PRIMARY KEY (id),
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (group_id) REFERENCES "groups" (id)
+);
