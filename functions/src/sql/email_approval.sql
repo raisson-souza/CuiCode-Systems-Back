4ebd7b45fd4c71b9f464f5f1e65d4b63e7137f0b
Raisson Souza <raissonrai@gmail.com>
2023-08-26 22:16:46 -0300

    - procedure aprovadora de email adicionada ao SQL; - tabela de aprovações de email atualizada no SQL.

--- functions/src/sql/email_approval.sql
+++ functions/src/sql/email_approval.sql
@@ -2,7 +2,46 @@ CREATE TABLE IF NOT EXISTS testing.email_approvals(
     id SERIAL PRIMARY KEY,
     user_id int NOT NULL,
     email varchar NOT NULL,
-    aproved bool NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "users" (id),
-    FOREIGN KEY (email) REFERENCES "users" (email)
-)
+    approved bool NOT NULL,
+    created timestamp DEFAULT now()::timestamp,
+    FOREIGN KEY (user_id) REFERENCES testing.users (id),
+    FOREIGN KEY (email) REFERENCES testing.users (email)
+);
+
+CREATE OR REPLACE PROCEDURE approve_user_email(db_stage varchar, _user_id int)
+LANGUAGE plpgsql AS 
+$$
+BEGIN 
+	IF db_stage = 'testing' THEN 
+		UPDATE testing.email_approvals em
+		SET approved = TRUE 
+		WHERE user_id = $2;
+
+		UPDATE testing.users
+		SET email_approved = TRUE 
+		WHERE id = $2;
+	
+	ELSEIF db_stage = 'staging' THEN 
+		UPDATE staging.email_approvals
+		SET approved = TRUE 
+		WHERE user_id = $2;
+
+		UPDATE staging.users
+		SET email_approved = TRUE 
+		WHERE id = $2;
+
+	ELSEIF db_stage = 'production' THEN 
+		UPDATE production.email_approvals
+		SET approved = TRUE 
+		WHERE user_id = $2;
+
+		UPDATE production.users
+		SET email_approved = TRUE 
+		WHERE id = $2;
+	
+	ELSE
+		RAISE info 'db_stage: % inválido.', db_stage;
+	
+	END IF;
+END;
+$$;
