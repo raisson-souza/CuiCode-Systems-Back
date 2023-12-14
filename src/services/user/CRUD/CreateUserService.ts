edb3a076feb88d0f66f808f44be096cb1efcd182
Raisson Souza <raissonrai@gmail.com>
2023-12-13 22:31:20 -0300

    - CreateUser incorporado em CreateUserService.

--- src/services/user/CRUD/CreateUserService.ts
+++ src/services/user/CRUD/CreateUserService.ts
@@ -1,14 +1,17 @@
+import crypto from "crypto-js"
+
 import Service from "../../../classes/Service"
 import User from "../../../classes/User"
+import Operation from "../../../classes/Operation"
 
 import SendApprovalEmailOperation from "../services/email/SendApprovalUserEmailOperation"
 
-import CreateUser from "../utilities/CreateUser"
 import ValidateUser from "../utilities/ValidateUser"
 
 import Send from "../../../functions/Responses"
 import EmailSender from "../../../functions/system/EmailSender"
 import IsUndNull from "../../../functions/IsUndNull"
+import ToSqlDate from "../../../functions/SQL/ToSqlDate"
 
 import EmailTitles from "../../../enums/EmailTitlesEnum"
 
@@ -43,7 +46,7 @@ class CreateUserService extends Service
 
             await Promise.resolve(ValidateUser(DB_connection, user, true))
                 .then(async () => {
-                    await Promise.resolve(CreateUser(DB_connection, user))
+                    await Promise.resolve(new CreateUserOperation(user, DB_connection).PerformOperation())
                         .then(async () => {
                             Send.Ok(RES, `Usuário ${ user.GenerateUserKey() } criado com sucesso.`, Action)
                             new EmailSender().Internal(EmailTitles.NEW_USER, user.GenerateUserKey())
@@ -65,6 +68,75 @@ class CreateUserService extends Service
         {
             this.DB_connection.end()
         }
+    }
+}
+
+/**
+ * Creates a user.
+ */
+class CreateUserOperation extends Operation
+{
+    async PerformOperation()
+    {
+        try
+        {
+            this.EncryptUserPassword(this.User!)
+
+            const {
+                User,
+                DB_connection
+            } = this
+
+            let query =
+            `
+                INSERT INTO users (${ this.GenerateUserFields() }) VALUES 
+                (
+                    '${ User!.Username }',
+                    '${ User!.Name }',
+                    ${ ToSqlDate(User!.BirthDate) },
+                    '${ User!.Email }',
+                    '${ User!.RecoveryEmail }',
+                    '${ User!.Phone }',
+                    '${ User!.Password }',
+                    '${ User!.PasswordHint }',
+                    '${ User!.PhotoBase64 }',
+                    ${ User!.Sex?.Value }
+                )
+            `
+
+            query.trim()
+
+            await DB_connection.query(query)
+                .then(() => {})
+                .catch(ex => {
+                    throw new Error(ex.message);
+                })
+        }
+        catch (ex)
+        {
+            throw new Error((ex as Error).message);
+        }
+    }
+
+    private GenerateUserFields() : string
+    {
+        return `
+            "username",
+            "name",
+            "birthdate",
+            "email",
+            "recovery_email",
+            "phone",
+            "password",
+            "password_hint",
+            "photo_base_64",
+            "sex"
+        `
+    }
+
+    private EncryptUserPassword(user : User) : void
+    {
+        user.Password = crypto.MD5(user.Password).toString()
     }
 }
 
