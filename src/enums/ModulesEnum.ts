0da1904247ccd00e790e9be11a0bdf8c740f9ca9
Raisson Souza <raissonrai@gmail.com>
2024-02-05 23:40:18 -0300

    - Pré-Implementação do enum de Módulos.

---
+++ src/enums/ModulesEnum.ts
@@ -0,0 +1,19 @@
+enum ModulesEnum
+{
+    Users = 1,
+    Groups = 2,
+    Board = 3,
+    Morfeus = 4,
+    Chats = 5,
+    Solicitations = 6,
+    Operational = 7,
+    Cron = 8,
+    Hestia = 9,
+    Minerva = 10,
+    Donation = 11,
+    CuiPoints = 12,
+    Anansi = 13,
+    Zeus = 14,
+}
+
+export default ModulesEnum
