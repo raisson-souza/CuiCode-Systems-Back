5e1841b69adc0b001ff82b99e4454e8ce22e236c
Raisson Souza <raissonrai@gmail.com>
2024-02-15 14:40:48 -0300

    - Edição de estilo especial no ParameterBase implementada.

--- src/classes/bases/ParametersBase.ts
+++ src/classes/bases/ParametersBase.ts
@@ -13,6 +13,19 @@ abstract class ParametersBase
 
         return parameters
     }
+
+    static async SetSpecialSystemStyle
+    (
+        db : Client,
+        action : boolean
+    ) : Promise<Parameters>
+    {
+        const query = `UPDATE parameters SET "special_system_style_on" = ${ action } WHERE "id" = 1`
+
+        await db.query(query)
+
+        return ParametersBase.Get(db)
+    }
 }
 
 export default ParametersBase
